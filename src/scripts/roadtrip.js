bb37fb83d7cceb42ceba24a4ba2edd4a941cf9ac
Dejai <dlfyfield@smcm.edu>
2019-04-18 00:52:24 -0400

    Account for editor information; Added video for each day

--- src/scripts/roadtrip.js
+++ src/scripts/roadtrip.js
@@ -1,7 +1,8 @@
 
 var roadTripVideos = { "order": [], "is_menu_open":false};
 
-function navbarToggle(){
+function navbarToggle()
+{
 	let navbar = document.getElementById("navbar_responsive");
 	let navbarOpen = document.getElementById("navbar_open");
 	let navbarClose = document.getElementById("navbar_close");
@@ -16,7 +17,8 @@ function navbarToggle(){
 	}
 }
 
-function toggleBackstory(){
+function toggleBackstory()
+{
 	let back = document.getElementById("backstory");
 	let video = document.getElementById("video_display_container");
 	let disp = back.style.display;
@@ -26,22 +28,16 @@ function toggleBackstory(){
 
 	back.style.display = showBack;
 	video.style.display = showVideo;
-
 }
+
 function clearSelected(){
 	var selected = document.getElementsByClassName("selected_day");
 	for (var x = 0; x < selected.length; x++){
 		selected[x].classList.remove("selected_day");
 	}
 }
-function selectMenuItem(dayName){
-	var day_name = dayName.toLowerCase().replace(" ", "_");
-	var menu_item = document.getElementById(day_name);
-	clearSelected();
-	menu_item.classList.add("selected_day");
-}
-
-function clickedDay(event){
+function clickedDay(event)
+{
 
 	if (roadTripVideos.is_menu_open){
 		document.getElementById("navbar_close").click();
@@ -52,12 +48,11 @@ function clickedDay(event){
 	
 	let id = event.target.id;
 	let name = event.target.innerHTML;
-	selectDay(id);
-	setTitle(name);
-
+	setVideoDetails(id, name);
 }
 
-function clickedVideoNav(event){
+function clickedVideoNav(event)
+{
 	let action = event.target.innerHTML;
 	let current_vid = document.getElementsByClassName("selected_day")[0].id;
 	var indx = roadTripVideos.order.indexOf(current_vid);
@@ -75,25 +70,41 @@ function clickedVideoNav(event){
 	document.getElementById(next_vid).click();
 }
 
-function setTitle(value){
-	let videoTitle = document.getElementById("current_video_title");
-	videoTitle.innerHTML = value;
+function selectMenuItem(dayName){
+	var day_name = dayName.toLowerCase().replace(" ", "_");
+	var menu_item = document.getElementById(day_name);
+	clearSelected();
+	menu_item.classList.add("selected_day");
 }
 
-function selectDay(dayName){
+function setVideoDetails(dayID, dayName){
 	var video_frame = document.getElementById("video_frame");
-	video_frame.src = roadTripVideos[dayName].url;
-	selectMenuItem(dayName);
+	let video_title = document.getElementById("current_video_title");
+	let video_time = document.getElementById("video_time");
+	let editor_name = document.getElementById("editor_name");
+	let video_description = document.getElementById("video_description");
+
+	let time_formatted = (roadTripVideos[dayID].time) ? (roadTripVideos[dayID].time) : "n/a";
+
+	video_title.innerHTML = dayName;
+	video_frame.src = roadTripVideos[dayID].url;	
+	editor_name.innerHTML = roadTripVideos[dayID].editor;	
+	video_time.innerHTML = time_formatted;
+	video_description.innerHTML = roadTripVideos[dayID].description;
+
+	selectMenuItem(dayID);
 }
 
-function setByClassName(className, value){
+function setByClassName(className, value)
+{
 	let elements = document.getElementsByClassName(className);
 	for (var x = 0; x < elements.length; x++){
 		elements[x].innerHTML = value;
 	}
 }
 
-function createListOfElements(tagName, xmlElements, destinationElement){
+function createListOfElements(tagName, xmlElements, destinationElement)
+{
 	let destElement = document.getElementById("days_menu_list");
 
 	for (var x = 0; x < xmlElements.length; x++){
@@ -112,7 +123,9 @@ function createListOfElements(tagName, xmlElements, destinationElement){
 		let parent = xmlEle.parentNode;
 		let url = parent.querySelectorAll("url")[0].innerHTML;
 		let editor = parent.querySelectorAll("editor")[0].innerHTML;
-		roadTripVideos[day_id] = {"url":url, "editor":editor};
+		let time = parent.querySelectorAll("time")[0].innerHTML;
+		let desc = parent.querySelectorAll("description")[0].innerHTML
+		roadTripVideos[day_id] = {"url":url, "editor":editor, "time":time, "description": desc};
 	}
 }
 
@@ -138,7 +151,9 @@ function setPageValues(xmlDoc){
 
 	let previewURL = xmldoc.querySelectorAll("preview url")[0].innerHTML;
 	let previewEditor = xmldoc.querySelectorAll("preview editor")[0].innerHTML;
-	roadTripVideos["preview"] = {"url": previewURL, "editor": previewEditor};
+	let previewTime = xmldoc.querySelectorAll("preview time")[0].innerHTML;
+	let previewDesc = "This is a snippet of our trip, somewhere in Wyoming!"
+	roadTripVideos["preview"] = {"url": previewURL, "editor": previewEditor, "time":previewTime, "description":previewDesc};
 	roadTripVideos["order"].unshift("preview");
 	clickedDay({"target":{"id":"preview", "innerHTML":"Preview"}});
 	console.log(roadTripVideos);
